0d292f296659ac515a37852109198e3178c3bddc
aaridouane <aaridouane@gmail.com>
2025-01-05 13:43:34 +0100

    fix histogram func for gray images

--- src/image_preprocessor.cpp
+++ src/image_preprocessor.cpp
@@ -198,44 +198,61 @@ bool ImagePreprocessor::unsharpMask(double sigma, double strength) {
 cv::Mat ImagePreprocessor::getHistogram() const {
     if (!checkImageLoaded()) return cv::Mat();
 
-    std::vector<cv::Mat> bgr_planes;
-    cv::split(image_, bgr_planes);
-
-    // Compute histograms
+    // Setup histogram parameters
     int histSize = 256;
     float range[] = { 0, 256 };
     const float* histRange = { range };
     bool uniform = true, accumulate = false;
-    cv::Mat b_hist, g_hist, r_hist;
-
-    cv::calcHist(&bgr_planes[0], 1, 0, cv::Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
-    cv::calcHist(&bgr_planes[1], 1, 0, cv::Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
-    cv::calcHist(&bgr_planes[2], 1, 0, cv::Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
-
-    // Draw histograms
+    
+    // Create output image
     int hist_w = 512, hist_h = 400;
     int bin_w = cvRound((double)hist_w/histSize);
     cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(0,0,0));
 
-    // Normalize histograms
-    cv::normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
-    cv::normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
-    cv::normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+    if (image_.channels() == 1) {
+        // Grayscale image
+        cv::Mat hist;
+        cv::calcHist(&image_, 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+        cv::normalize(hist, hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
 
-    // Draw for each channel
-    for(int i = 1; i < histSize; i++) {
-        cv::line(histImage, 
-                 cv::Point(bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1))),
-                 cv::Point(bin_w*(i), hist_h - cvRound(b_hist.at<float>(i))),
-                 cv::Scalar(255, 0, 0), 2, 8, 0);
-        cv::line(histImage, 
-                 cv::Point(bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1))),
-                 cv::Point(bin_w*(i), hist_h - cvRound(g_hist.at<float>(i))),
-                 cv::Scalar(0, 255, 0), 2, 8, 0);
-        cv::line(histImage, 
-                 cv::Point(bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1))),
-                 cv::Point(bin_w*(i), hist_h - cvRound(r_hist.at<float>(i))),
-                 cv::Scalar(0, 0, 255), 2, 8, 0);
+        // Draw histogram in white
+        for(int i = 1; i < histSize; i++) {
+            cv::line(histImage, 
+                     cv::Point(bin_w*(i-1), hist_h - cvRound(hist.at<float>(i-1))),
+                     cv::Point(bin_w*(i), hist_h - cvRound(hist.at<float>(i))),
+                     cv::Scalar(255, 255, 255), 2, 8, 0);
+        }
+    } else {
+        // Color image
+        std::vector<cv::Mat> bgr_planes;
+        cv::split(image_, bgr_planes);
+
+        // Compute histograms for each channel
+        cv::Mat b_hist, g_hist, r_hist;
+        cv::calcHist(&bgr_planes[0], 1, 0, cv::Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
+        cv::calcHist(&bgr_planes[1], 1, 0, cv::Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+        cv::calcHist(&bgr_planes[2], 1, 0, cv::Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
+
+        // Normalize histograms
+        cv::normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+        cv::normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+        cv::normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+
+        // Draw for each channel
+        for(int i = 1; i < histSize; i++) {
+            cv::line(histImage, 
+                     cv::Point(bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1))),
+                     cv::Point(bin_w*(i), hist_h - cvRound(b_hist.at<float>(i))),
+                     cv::Scalar(255, 0, 0), 2, 8, 0);
+            cv::line(histImage, 
+                     cv::Point(bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1))),
+                     cv::Point(bin_w*(i), hist_h - cvRound(g_hist.at<float>(i))),
+                     cv::Scalar(0, 255, 0), 2, 8, 0);
+            cv::line(histImage, 
+                     cv::Point(bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1))),
+                     cv::Point(bin_w*(i), hist_h - cvRound(r_hist.at<float>(i))),
+                     cv::Scalar(0, 0, 255), 2, 8, 0);
+        }
     }
 
     return histImage;
